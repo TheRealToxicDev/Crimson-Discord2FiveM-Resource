fa1477992c07b9ea68c351461d4193ca9fe47f88
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2020-02-11 15:19:52 -0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+-- If you are using the >>NEWEST VERSION<< of the DiscordBot Resource, set this to true
+UsingDiscordBot = true
+
+-- Change 'false' to 'true' to send kick messages into the chat
+SendKickToChat = false
+
+-- Change 'false' to 'true' to send ban messages into the chat
+SendBanToChat = false
+
+-- Change this to change the ban duration (Infinite = 0, One Minute ~ 0.017, One Hour = 1, Two Hours = 2, Three Hours = 3, etc.)
+BanDuration = 0.025
