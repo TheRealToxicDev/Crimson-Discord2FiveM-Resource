63b7d5c8a466822884bb46b0742954e328773cb3
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2020-02-11 15:27:22 -0700

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -7,5 +7,5 @@ SendKickToChat = false
 -- Change 'false' to 'true' to send ban messages into the chat
 SendBanToChat = false
 
--- Change this to change the ban duration (Infinite = 0, One Minute ~ 0.017, One Hour = 1, Two Hours = 2, Three Hours = 3, etc.)
+-- Change this to change the ban duration (Infinite = 0, One Minute = 0.017, One Hour = 1, Two Hours = 2, Three Hours = 3, etc.)
 BanDuration = 0.025
